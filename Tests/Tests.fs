c539a20f5d28c2e53d7b96091e95e95ebe788893
Patrick Drechsler <patrick@pdrechsler.de>
2022-01-12 20:18:16 +0100

    make hello world test signature more idiomatic

--- Tests/Tests.fs
+++ Tests/Tests.fs
@@ -14,7 +14,7 @@ open Xunit
 // InlineData example
 [<Theory>]
 [<InlineData(1,42,43)>]
-let ``xunit inlinedata hello world`` (a : int, b : int, expected : int) =
+let ``xunit inlinedata hello world`` (a : int) (b : int) (expected : int) =
     let actual = a + b
     Assert.Equal(expected, actual)
 
